79d18cfbeb9295c590ba07abe4fb2472f9cb96b3
Dramane <Dramane.dev@gmail.com>
2021-08-18 21:48:42 +0200

    Front: Construction of home page part 1

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,42 @@
+import React, { Component } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>LIBRARY APP</h1>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="top-bar">
+          <Link to={"/books"}>
+            <h1 className="title">Library App 📓</h1>
+          </Link>
+          <Link to={"/book"}>
+            <p id="add-new-book" className="top-bar-buttons">Add new book</p>
+          </Link>
+          <div className="authentication-buttons">
+          <Link to={"/signin"}>
+            <p id="signin" className="top-bar-buttons">Sign In</p>
+          </Link>
+          <Link to={"/signup"}>
+            <p id="signup" className="top-bar-buttons">Sign Up</p>
+          </Link>
+          </div>
+        </div>
+
+        <div id="not-logged" className="not-logged">
+            <p>Could you <b>loggin</b></p>
+            <p>To have an access into the Library App</p>
+            <p>
+              <span id="signin" class="signin">Sign In</span>
+              or
+              <span id="signup" class="signup">Sign Up</span>  
+            </p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
